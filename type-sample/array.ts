6c4bd9d9b9c0dfbb0c478e35351bee373f3a94f5
sjh9391985 <sjh9391985@gmail.com>
2023-07-17 15:29:22 +0900

    feat: 'primitive type'

---
+++ type-sample/array.ts
@@ -0,0 +1,9 @@
+let nums: number[] = [1, 2, 3]; // 오직 숫자만 허용하는 배열
+
+let strs: string[] = ['가', '나', '다']; // 오직 문자만 허용하는 배열
+
+let bools: boolean[] = [true, false, false]; // 오직 불리언만 허용하는 배열
+
+let anys: any[] = [1, '가', true]; // 모든 원시 데이터 타입 허용하는 배열
+
+let selects: (number | string)[] = [1, '가']; // 특정 데이터 타입 허용하는 배열
---
+++ type-sample/primitive.ts
@@ -0,0 +1,10 @@
+// 원시 데이터 타입에는 number, string, boolean 이 있음
+
+// 명시적 설정
+let num: number = 12345;
+let str: string = '가나다';
+let bool: boolean = true;
+
+console.log('num : ', num);
+console.log('str : ', str);
+console.log('bool : ', bool);
